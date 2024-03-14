7eb7c7ce0adbdf81c610c896e152ea07fc1fcadf
renox <planetrenox@protonmail.com>
2024-03-13 17:54:47 -0700

    0.2.0

--- readme.md
+++ readme.md
@@ -1,46 +1,24 @@
 [available on npmjs](https://www.npmjs.com/package/cute-fs)
 # cute-fs
+This package **_solves_** the issue of file writes being confusing with vague syntax. With cute-fs, dirs in the middle are always created if they don't exist. soft.write() does not overwrite but hard.overwrite() will always write. You can also mix unix/windows slashes.
 
 
-A tiny description.
+### Install
 
 
-## Features
+```
+npm install -g cute-fs
+```
 
 
-- Item
-
-- Item
-
-- Item
+### Use
 
 
-## Why
+```JavaScript
+import URL from 'cute-fs';
+URL('C:\\WindowsLikePathOr/LinuxLike/Path.txt').soft.write(`any`);
+URL('path/to/file.txt').hard.overwrite(`any`);
+```
 
+We use URL because we want cute-fs to be universal. Soon we will add ability to read which will include local and web.
 
-This package solves ...
-
-
-## Getting Started
-
-
-### Installation
-
-
-1. Install cute-fs locally.
-
-
-   ```
-   npm install cute-fs
-   npm install --global cute-fs
-   ```
-
-
-### Usage
-
-
-   ```JavaScript
-   import default from 'cute-fs';
-   ```
-   
-![image of banner](https://fakeimg.pl/730x380)
--- src/index.js
+++ src/index.js
@@ -1,114 +1,29 @@
 import fs from 'fs';
 import path from 'path';
-import axios from 'axios';
-class UrlHandler
+export default function URL(filePath)
 {
-    constructor()
+    const softWrite = (content) =>
     {
-        this._isHard = false;
-    }
-
-    get soft()
-    {
-        this._isHard = false;
-        return this;
-    }
-
-    get hard()
-    {
-        this._isHard = true;
-        return this;
-    }
-
-    url(inputPath)
-    {
-        this.basePath = inputPath.startsWith('/') ? path.resolve(inputPath) : inputPath;
-        return this;
-    }
-
-    async write(content)
-    {
-        const ensureDir = (dirPath) =>
-        {
-            if (!fs.existsSync(dirPath)) {
-                fs.mkdirSync(dirPath, {recursive: true});
-            }
-        };
-
-        if (this.basePath.includes('://')) {
-            throw new Error('Writing to a web URL is not supported.');
+        const directory = path.dirname(filePath);
+        fs.mkdirSync(directory, {recursive: true});
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, content);
         }
-        else {
-            const fullPath = path.resolve(this.basePath);
-            if (fullPath.endsWith('/')) {
-                ensureDir(fullPath);
-            }
-            else {
-                ensureDir(path.dirname(fullPath));
-                if (this.isHard || !fs.existsSync(fullPath)) {
-                    fs.writeFileSync(fullPath, content);
-                }
-            }
-        }
+    };
 
-        return this;
-    }
-
-    async overwrite(content)
+    const hardOverwrite = (content) =>
     {
-        this.isHard = true; // Force hard behavior for overwrite
-        return this.write(content);
-    }
+        const directory = path.dirname(filePath);
+        fs.mkdirSync(directory, {recursive: true});
+        fs.writeFileSync(filePath, content);
+    };
 
-    async read()
-    {
-        const readDirOrFile = (dirOrFilePath) =>
-        {
-            if (fs.existsSync(dirOrFilePath)) {
-                if (fs.lstatSync(dirOrFilePath).isDirectory()) {
-                    return fs.readdirSync(dirOrFilePath);
-                }
-                else {
-                    return fs.readFileSync(dirOrFilePath, 'utf-8');
-                }
-            }
-            else if (this.isHard) {
-                if (dirOrFilePath.endsWith('/')) {
-                    fs.mkdirSync(dirOrFilePath, {recursive: true});
-                }
-                else {
-                    const dirPath = path.dirname(dirOrFilePath);
-                    fs.mkdirSync(dirPath, {recursive: true});
-                    fs.writeFileSync(dirOrFilePath, '');
-                }
-                return '';
-            }
-            throw new Error('Path does not exist.');
-        };
-
-        if (this.basePath.includes('://')) {
-            // Handle web URL
-            try {
-                const response = await axios.get(this.basePath);
-                return response.data;
-            }
-            catch (error) {
-                if (this.isHard) {
-                    // In hard mode, attempt to create resources or return an empty structure
-                    return '';
-                }
-                throw error;
-            }
-        }
-        else {
-            return readDirOrFile(path.resolve(this.basePath));
-        }
-    }
-}
-
-// Exposing a function directly to align with the specification
-export function url(inputPath)
-{
-    const handler = new UrlHandler();
-    return handler.url(inputPath);
+    return {
+        soft: {
+            write: softWrite,
+        },
+        hard: {
+            overwrite: hardOverwrite,
+        },
+    };
 }
